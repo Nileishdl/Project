c60283dea0945e2047aba8ea9e97caaa41936fdd
unknown <nlimbalkar1@gmail.com>
2023-04-10 18:56:54 +0530

    Done

--- Project/src/com/project/group/L/testingdemo.java
+++
@@ -1,7 +0,0 @@
-package com.project.group.L;
-
-public class testingdemo {
-public static void main(String[] args) {
-	System.out.println("aale");
-}
-}
--- Project/src/com/project/group/L/testingpurpose.java
+++
@@ -1,9 +0,0 @@
-package com.project.group.L;
-
-public class testingpurpose {
-
-	
-	public static void main(String[] args) {
-		System.out.println("Jaml n bho");
-	}
-}
